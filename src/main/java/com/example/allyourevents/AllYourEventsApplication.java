74066847f26dc065bbf5aebdd59cf8836333e8bc
Salvatore <salvatore.piemonte7@gmail.com>
2022-06-11 15:47:11 +0200

    Progetto Masterclass Code To Work

---
+++ src/main/java/com/example/allyourevents/AllYourEventsApplication.java
@@ -0,0 +1,13 @@
+package com.example.allyourevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllYourEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllYourEventsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/allyourevents/AllYourEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.allyourevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllYourEventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
