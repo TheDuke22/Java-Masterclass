6807db44ee13ec7de143abafc7613688569a36b6
TheDuke22 <djdino2209@gmail.com>
2022-06-14 15:33:53 +0200

    aggiunta CRUD

--- src/main/java/com/example/allyourevents/AllYourEventsApplication.java
+++ src/main/java/com/example/allyourevents/AllYourEventsApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class AllYourEventsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AllYourEventsApplication.class, args);
     }
-//commento  //ok
+
 }
